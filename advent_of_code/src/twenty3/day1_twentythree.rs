d487f692cbbd77843326f6b6078a2c57f8675427
laffed <roarklaffed@gmail.com>
2023-12-02 13:44:18 -0600

    .

--- advent_of_code/src/twenty3/day1_twentythree.rs
+++ advent_of_code/src/twenty3/day1_twentythree.rs
@@ -81,7 +81,43 @@ where
         })
         .sum()
 }
-
+fn filter_digits(line: &str) -> Vec<u32> {
+    line.chars()
+        .enumerate()
+        .filter_map(|(i, c)| {
+            c.to_digit(10).or_else(|| match c {
+                'o' => map(&line[i..], "one", 1),
+                't' => map(&line[i..], "two", 2).or_else(|| map(&line[i..], "three", 3)),
+                'f' => map(&line[i..], "four", 4).or_else(|| map(&line[i..], "five", 5)),
+                's' => map(&line[i..], "six", 6).or_else(|| map(&line[i..], "seven", 7)),
+                'e' => map(&line[i..], "eight", 8),
+                'n' => map(&line[i..], "nine", 9),
+                _ => None,
+            })
+        })
+        .collect()
+}
+fn map(l: &str, s: &str, n: u32) -> Option<u32> {
+    if l.len() < s.len() {
+        return None;
+    }
+    if s == &l[..s.len()] {
+        return Some(n);
+    }
+    None
+}
+fn aoc_1_2(data: &str) -> usize {
+    let mut sum = 0;
+    for line in data.split_whitespace() {
+        let v = filter_digits(line);
+        // for correctness, unnecessary with the supplied data
+        if v.is_empty() {
+            continue;
+        }
+        sum += v[0] * 10 + v.last().unwrap();
+    }
+    sum as usize
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -103,21 +139,8 @@ mod tests {
 
     #[test]
     fn p2_solution() {
-        assert_eq!(solution_p2("inputs/twenty3/d1.txt"), 0);
-    }
-
-    #[test]
-    fn test_test() {
-        assert_eq!(helper("1abc2", false), 1);
-        assert_eq!(helper("1abc2", true), 2);
-        assert_eq!(helper("pqr3stu8vwx", false), 3);
-        assert_eq!(helper("pqr3stu8vwx", true), 8);
-        assert_eq!(helper("a1b2c3d4e5f", false), 1);
-        assert_eq!(helper("a1b2c3d4e5f", true), 5);
-        assert_eq!(helper("treb7uchet", false), 7);
-        assert_eq!(helper("treb7uchet", true), 7);
-        // assert_eq!(helper("pqr3stu8vwx"), Some(38));
-        // assert_eq!(helper("a1b2c3d4e5f"), Some(15));
-        // assert_eq!(helper("treb7uchet"), Some(77));
+        // assert_eq!(solution_p2("inputs/twenty3/d1.txt"), 0);
+        let data = std::fs::read_to_string("inputs/twenty3/d1.txt").unwrap();
+        assert_eq!(aoc_1_2(&data), 0);
     }
 }
